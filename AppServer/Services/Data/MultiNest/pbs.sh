4cd192cd3cf1fd23648a82f4d07dff28a8104d9a
Konrad Klimaszewski <konrad.klimaszewski@ncbj.gov.pl>
2013-05-09 12:19:37 +0200

    Proper implementation of MultiNest service.

--- AppServer/Services/Data/MultiNest/pbs.sh
+++ AppServer/Services/Data/MultiNest/pbs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#PBS -l nodes=@@{PBS_NODES}:ppn=@@{PBS_PPN}
 
-./python/run.py
+cd $PBS_O_WORKDIR
+mkdir chains
 
+source /mnt/home/kklimaszewski/MultiNest/PyMultiNest/setup.sh
+
+/mnt/home/kklimaszewski/MultiNest/PyMultiNest/MultinestEvaluator.py 2>&1 | tee progress.log 
+
+exit $?
