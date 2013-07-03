54f65141d41769d12b5bb6338bfc09ad7442a375
Konrad Klimaszewski <konrad.klimaszewski@ncbj.gov.pl>
2013-07-03 15:14:54 +0200

    Switch AppServer production setup to apprunner account.

--- Services/Data/MultiNest/pbs.sh
+++ Services/Data/MultiNest/pbs.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+#PBS -N MultiNest
 
+echo "#####" | tee -a progress.log
+echo "Start: $(date)" | tee -a progress.log
+
+echo "Inicjalizajca ..." | tee -a progress.log
 cd $PBS_O_WORKDIR
 mkdir chains
 
-source /mnt/home/kklimaszewski/MultiNest/PyMultiNest/setup.sh
+source $HOME/MultiNest/PyMultiNest/setup.sh
 
-/mnt/home/kklimaszewski/MultiNest/PyMultiNest/MultinestEvaluator.py 2>&1 | tee progress.log 
+echo "#####" | tee -a progress.log
+echo "Obliczenia ..." | tee -a progress.log
+$HOME/MultiNest/PyMultiNest/MultinestEvaluator.py 2>&1 | tee -a progress.log 
 
+echo "#####" | tee -a progress.log
+echo "Stop: $(date)" | tee -a progress.log
 exit $?
