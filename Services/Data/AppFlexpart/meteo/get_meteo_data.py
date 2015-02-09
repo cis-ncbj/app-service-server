05a3908e6704711eee2cdaa926c79392b2f6dec7
Krzysztof Gomulski <krzysztof.gomulski@ncbj.gov.pl>
2015-02-09 16:00:02 +0100

    another fixes for server definition

--- Services/Data/AppFlexpart/meteo/get_meteo_data.py
+++ Services/Data/AppFlexpart/meteo/get_meteo_data.py
@@ -8,56 +8,58 @@ start_date = '@@{start_case}'
 end_date = '@@{end_case}'
 out_file_name = 'AVAILABLE'
 
+
 def get_in_data_file(forecast):
-    return "wrfout_d02_"+forecast.strftime('%Y-%m-%d_%H')
-   
+    return "wrfout_d02_" + forecast.strftime('%Y-%m-%d_%H')
+
 
 def link_in_data_file(forecast):
-    if not os.path.isfile(get_in_data_file(forecast)+".nc"):
-         os.symlink( os.path.join(forecast_dir,
-                                 forecast.strftime('CIS.%Y%m%d%H'),
+    if not os.path.isfile(get_in_data_file(forecast) + ".nc"):
+        os.symlink(os.path.join(forecast_dir,
+                                (forecast + timedelta(hours=12)).strftime('CIS.%Y%m%d%H'),
                                 'wrfprd',
-                                "wrfout_d02_"+(forecast-timedelta(hours=12)).strftime('%Y-%m-%d_%H:%M:%S')),
-            get_in_data_file(forecast)+".nc")
+                                "wrfout_d02_" + forecast.strftime('%Y-%m-%d_%H:%M:%S')),
+                   get_in_data_file(forecast) + ".nc")
+
 
 def main():
-    av_forecasts=[]
-    s_date = datetime.strptime(start_date,'%Y%m%d %H0000')
-    e_date = datetime.strptime(end_date,'%Y%m%d %H0000')
-    
+    av_forecasts = []
+    s_date = datetime.strptime(start_date, '%Y%m%d %H0000')
+    e_date = datetime.strptime(end_date, '%Y%m%d %H0000')
+
     for dir_ in os.listdir(forecast_dir):
-        av_forecasts.append(datetime.strptime(dir_,'CIS.%Y%m%d%H'))
+        av_forecasts.append(datetime.strptime(dir_, 'CIS.%Y%m%d%H'))
     av_forecasts = sorted(av_forecasts)
     forecast_index = None
-    
-    for i,date in enumerate(av_forecasts):
-        if s_date>=(date) and s_date<=(av_forecasts[i+1]):
-            forecast_index = i
+
+    for i, date in enumerate(av_forecasts):
+        if date <= s_date <= (av_forecasts[i + 1]):
+            forecast_index = i + 1
             break
-    
+
     step = timedelta(hours=1)
     tmpdate = s_date
-    lines = []
 
     in_data = get_in_data_file(av_forecasts[forecast_index])
     link_in_data_file(av_forecasts[forecast_index])
-   
-    out_file = open(out_file_name ,'w')
+
+    out_file = open(out_file_name, 'w')
     header = '''XXXXXX EMPTY LINES XXXXXXXXX
 XXXXXX EMPTY LINES XXXXXXXX
 YYYYMMDD HHMMSS   name of the file(up to 80 characters)
 '''
     out_file.write(header)
-    
-    while tmpdate<e_date:
-        if tmpdate >= (av_forecasts[forecast_index+1]):
-            forecast_index +=1
+
+    while tmpdate < e_date:
+        if tmpdate >= (av_forecasts[forecast_index + 1]):
+            forecast_index += 1
             in_data = get_in_data_file(av_forecasts[forecast_index])
             link_in_data_file(av_forecasts[forecast_index])
 
-        out_file.write(tmpdate.strftime('%Y%m%d %H0000')+"      '"+in_data+".nc'      ' '\n")
+        out_file.write(tmpdate.strftime('%Y%m%d %H0000') + "      '" + in_data + ".nc'      ' '\n")
         tmpdate += step
-    out_file.write(e_date.strftime('%Y%m%d %H0000')+ "      '"+in_data+".nc'      ' '\n")
-    
-if __name__=='__main__':
-  main()
+    out_file.write(e_date.strftime('%Y%m%d %H0000') + "      '" + in_data + ".nc'      ' '\n")
+
+
+if __name__ == '__main__':
+    main()
--- Services/Data/AppFlexpart/pbs.sh
+++ Services/Data/AppFlexpart/pbs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #PBS -l nodes=1:ppn=10,walltime=5:00:00
-#PBS -q i12h
 #PBS -l mem=6GB
 
 #setenv LD_LIBRARY_PATH /mnt/opt/tools/slc6/binutils/2.22/lib:/mnt/opt/tools/slc6/netcdf/4.3.0/lib
@@ -11,11 +10,11 @@ module load netcdf/4.3.0-pgi
 
 cd $PBS_O_WORKDIR
 
-ln -s /mnt/home/mjkorycki/models/slc6/FLEXPART_WRF/src_flexwrf_v3.1/data/IGBP_int1.dat $PBS_O_WORKDIR/IGBP_int1.dat
-ln -s /mnt/home/mjkorycki/models/slc6/FLEXPART_WRF/src_flexwrf_v3.1/data/OH_7lev_agl.dat $PBS_O_WORKDIR/OH_7lev_agl.dat
-ln -s /mnt/home/mjkorycki/models/slc6/FLEXPART_WRF/src_flexwrf_v3.1/data/surfdata.t $PBS_O_WORKDIR/surfdata.t
-ln -s /mnt/home/mjkorycki/models/slc6/FLEXPART_WRF/src_flexwrf_v3.1/data/surfdepo.t $PBS_O_WORKDIR/surfdepo.t
-ln -s /mnt/home/mjkorycki/models/slc6/FLEXPART_WRF/src_flexwrf_v3.1/flexwrf31_pgi_omp $PBS_O_WORKDIR/flexwrf31_pgi_omp
+ln -s /mnt/home/mjkorycki/models/slc6/FLEXPART_WRF/src_flexwrf_v3.1/data/IGBP_int1.dat $PWD/IGBP_int1.dat
+ln -s /mnt/home/mjkorycki/models/slc6/FLEXPART_WRF/src_flexwrf_v3.1/data/OH_7lev_agl.dat $PWD/OH_7lev_agl.dat
+ln -s /mnt/home/mjkorycki/models/slc6/FLEXPART_WRF/src_flexwrf_v3.1/data/surfdata.t $PWD/surfdata.t
+ln -s /mnt/home/mjkorycki/models/slc6/FLEXPART_WRF/src_flexwrf_v3.1/data/surfdepo.t $PWD/surfdepo.t
+ln -s /mnt/home/mjkorycki/models/slc6/FLEXPART_WRF/src_flexwrf_v3.1/flexwrf31_pgi_omp $PWD/flexwrf31_pgi_omp
 #chmod 777 *.dat
 #chmod 777 *.t
 
@@ -25,13 +24,14 @@ python get_meteo_data.py
 cd ..
 
 echo "=====================FORMER PATHNAMES FILE===================" > flexwrf.input
-echo "$PBS_O_WORKDIR/output/" >> flexwrf.input
-echo "$PBS_O_WORKDIR/meteo/" >> flexwrf.input
-echo "$PBS_O_WORKDIR/meteo/AVAILABLE" >> flexwrf.input
+echo "$PWD/output/" >> flexwrf.input
+echo "$PWD/meteo/" >> flexwrf.input
+echo "$PWD/meteo/AVAILABLE" >> flexwrf.input
 echo "=============================================================" >> flexwrf.input
 cat input >> flexwrf.input
 
 export OMP_NUM_THREADS=10
+ulimit -s unlimited
 #limit stacksize unlimited
-
+pwd
 ./flexwrf31_pgi_omp flexwrf.input
