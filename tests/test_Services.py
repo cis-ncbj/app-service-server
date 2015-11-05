7676ad6742bf08c70035ce57060e0ca7a1722d68
Konrad Klimaszewski <Konrad.Klimaszewski@ncbj.gov.pl>
2015-11-05 08:31:39 +0100

    Refactor unit tests to take input data from json files.

--- tests/test_Services.py
+++ tests/test_Services.py
@@ -14,22 +14,17 @@ def setup_module():
     :return:
     """
     test_assets = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets')
-    # from Config import conf
     # change directory from which job data are loaded
     conf.gate_path_jobs = os.path.join(test_assets, 'payloads')
     conf.service_path_conf = os.path.join(test_assets, 'services')
     conf.service_path_data = os.path.join(test_assets, 'services','Data')
+    conf.test_path_data = os.path.join(test_assets, 'data')
     G.init()
-    # add test services to services list
-    #for _service in ['test', 'default']:
-    #    with open(os.path.join(test_assets, 'services', _service + '.json')) as _f:
-    #        ServiceStore[_service] = Service(_service, conf.json_load(_f))
 
 
 class TestValidator:
     @classmethod
     def setup_class(cls):
-        cls.valid_job = Job('test_valid_job.json')
         cls.service = G.SERVICE_STORE['test']
 
     def test_validate(self):
@@ -37,11 +32,10 @@ class TestValidator:
         Validator.validate for proper input
         :return:
         """
-        # job = Job('test_valid_job.json')
-        # print self.valid_job.id()
-        G.VALIDATOR.validate(self.valid_job)
+        job = Job('test_valid_job.json')
+        G.VALIDATOR.validate(job)
 
-        result = self.valid_job.data.data
+        result = job.data.data
         valid_result = {
             "test_date": "20150317 135200",
             "test_object": {
@@ -78,100 +72,75 @@ class TestValidator:
 
     # TODO more tests with different kinds of invalid payloads
 
-    def test_validate_value_date(self):
+    def test_validate_value_success(self):
         """
-        Validator.validate_value:  datetime variable
-        :return:
+        Generate check_validate_value_success tests based on json data
+        :return: generator of check_validate_value_success tests for nose
         """
-        var_name = ['test_date']
-        template = self.service.variables[var_name[0]]
-        # correct date input
-        eq_(G.VALIDATOR.validate_value(var_name, '20150317 135200', template),
-            '20150317 135200', 'Proper date')
-        # returns 2003-4-4 13:52 ... is that correct? lets keep it that way
-        eq_(G.VALIDATOR.validate_value(var_name, '200344 135200', template),
-            '200344 135200', 'Month and day without leading zeros')
+        name = os.path.join(conf.test_path_data, "validate_value_success.json")
+        with open(name) as data_file:
+            data = conf.json_load(data_file)
 
-        # failed date input
-        # Too short date
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, '201503 135200', template)
-        # Day out of bounds
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, '20150344 135200', template)
-        # Hour our of bounds
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, '20150317 995200', template)
-        # Month out of bound
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, '20151317 005200', template)
-        # Random string
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, '201w 005200', template)
+        for item in data:
+            item.append("%s: %s != %s" % tuple(item))
+            item.insert(0, self.check_validate_value_success)
+            yield tuple(item)
 
-    def test_validate_value_float_array(self):
+    def test_validate_value_failure(self):
+        """
+        Generate check_validate_value_failure tests based on json data
+        :return: generator of check_validate_value_failure tests for nose
+        """
+        name = os.path.join(conf.test_path_data, "validate_value_failure.json")
+        with open(name) as data_file:
+            data = conf.json_load(data_file)
+
+        for item in data:
+            item.insert(0, self.check_validate_value_failure)
+            yield tuple(item)
+
+    def check_validate_value_success(self, name, value, result, desc):
+        """
+        Test Validator.validate_value with correct payload
+        :param name: name of variable to test
+        :param value: payload
+        :param result: expected result
+        :param desc: description
+        """
+        template = self.service.variables[name]
+        # proper values
+        eq_(G.VALIDATOR.validate_value([name], value, template), result, desc)
+
+    def check_validate_value_failure(self, name, value):
+        """
+        Test Validator.validate_value with incorrect payload
+        :param name: name of variable to test
+        :param value: payload
+        :param desc: description
+        """
+        template = self.service.variables[name]
+        # bad values
+        assert_raises(ValidatorError, G.VALIDATOR.validate_value, [name], value, template)
+
+    # Special cases
+    def test_validate_value_float_tuple(self):
         """
         Validator.validate_value:  float_array variable
         :return:
         """
-        var_name = ['test_float_array']
-        template = self.service.variables[var_name[0]]
-        # proper value
-        eq_(G.VALIDATOR.validate_value(var_name, [0.1, 55.3, 2.3], template),
-            [0.1, 55.3, 2.3], "Basic value - array")
-        eq_(G.VALIDATOR.validate_value(var_name, (0.1, 55.3, 2.3), template),
-            [0.1, 55.3, 2.3], "Basic value - tuple")
+        var_name = 'test_float_array'
+        template = self.service.variables[var_name]
+        # proper value in a tuple
+        input = (0.1, 55.3, 2.3)
+        output = [0.1, 55.3, 2.3]
+        eq_(G.VALIDATOR.validate_value([var_name], input, template),
+            output, 'test_float_array: %s != %s' % (input, output))
 
-        # failed input
-        # Number instead of array: failed up to commit #106aa0669
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, 0.1, template)
-        # Too many values
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, (0.1, 55.3, 2.3, 33.21, 5.1, 7.5),
-                      template)
-        # One value out of bounds
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, (599999999.1, 7.5), template)
-        # One value out of bounds
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, (5.1, -7.5), template)
-
-    def test_validate_value_float(self):
-        """
-        G.VALIDATOR.validate_value:  float variable
-        :return:
-        """
-        var_name = ['test_float']
-        template = self.service.variables[var_name[0]]
-        # correct value
-        eq_(G.VALIDATOR.validate_value(var_name, 0.1, template),
-            0.1, "Proper value")
-        eq_(G.VALIDATOR.validate_value(var_name, '0.1', template),
-            0.1, 'Float as a string')
-
-        # failed input
-        # Random string
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, "ss", template)
-        # localization? Nope and lets keep it that way
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, "0,1", template)
-
-    def test_validate_value_object(self):
+    def test_validate_value_nested_object(self):
         """
         G.VALIDATOR.validate_value:  object variable
         :return:
         """
-        var_name = ['test_object']
-        template = self.service.variables[var_name[0]]
-        # proper values
-        eq_(G.VALIDATOR.validate_value(
-            var_name, dict(A=2, B=[4, 5, 34]), template),
-            dict(A=2, B=[4, 5, 34], C="20151115 112000"), "Basic object")
-        eq_(G.VALIDATOR.validate_value(
-            var_name, {}, template),
-            dict(A=1, B=[2, 3, 4], C="20151115 112000"), "Empty object")
-
-        # failed input
-        # Unsupported variable
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, dict(C='bla'), template)
-        # Array instead of dict
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, [], template)
-        # Not valid variable
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, dict(A=99999999), template)
-        # Not valid variable
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, dict(B=[1, 2, 3, 4, 5, 4]), template)
-
         # Too nested object
         template = dict(
             type="object",
@@ -203,48 +172,13 @@ class TestValidator:
                           )),
                       template)
 
-    def test_validate_value_object_array(self):
-        """
-        G.VALIDATOR.validate_value:  object_array variable
-        :return:
-        """
-        var_name = ['test_object_array']
-        template = self.service.variables[var_name[0]]
-        # proper values
-        eq_(G.VALIDATOR.validate_value(var_name,
-                                          [dict(K=2, L="21011119 133010"),
-                                           dict(K=0.2, L="20050119 033010"),
-                                           dict(K=20.1, L="22011116 103110")],
-                                          template),
-            [
-                dict(K=2, L="21011119 133010"),
-                dict(K=0.2, L="20050119 033010"),
-                dict(K=20.1, L="22011116 103110")
-            ],
-            "Basic object")
-        eq_(G.VALIDATOR.validate_value(var_name, [{}, {}], template),
-            [dict(K=1.2, L="20011119 103010"), dict(K=1.2, L="20011119 103010")],
-            "Empty objects")
-        eq_(G.VALIDATOR.validate_value(var_name, [{}, dict(K=2, L="21011119 133010")],
-                                          template),
-            [dict(K=1.2, L="20011119 103010"), dict(K=2, L="21011119 133010")],
-            "Mixed empty and full objects")
-
-        # failed input
-        # Object instead of array
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, {'ss': 1}, template)
-        # One failed value in one object in array
-        assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name,
-                      [dict(K=2, L="21011119 133010"),
-                       dict(K=0.2, L="20050119 433010"),  # hour out of bounds
-                       dict(K=20.1, L="22011116 103110")],
-                      template)
-
     def test_validate_value_unknown_type(self):
         """
-        G.VALIDATOR.validate_value:  service providing unsupported variable type
+        validate_value:  service providing unsupported variable type
         :return:
         """
+        # Let us leave it as external case not parsed by Service so that when Service will validate input this test
+        # still can be performed
         var_name = ['test_mistaken']  # service dev has misspelled 'float' and leaves 'flaot' in service file
         template = {
             'type': "flaot",
@@ -254,3 +188,4 @@ class TestValidator:
         # failing up to commit #106aa0669
         # Service developer messed up variable type and 'hacker' noticed
         assert_raises(ValidatorError, G.VALIDATOR.validate_value, var_name, "Hack payload", template)
+
